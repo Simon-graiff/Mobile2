613c286c222adb9488138a4ebe0223ce9373dd9e
Simon Graiff <s.graiff@outlool.com>
2015-06-01 21:17:34 +0200

    new app icon

--- app/src/main/java/dhbw/mobile2/NavDrawerListAdapter.java
+++ app/src/main/java/dhbw/mobile2/NavDrawerListAdapter.java
@@ -34,21 +34,6 @@ public class NavDrawerListAdapter extends BaseAdapter {
     }
 
     @Override
-    public int getCount() {
-        return navDrawerItems.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return navDrawerItems.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             LayoutInflater mInflater = (LayoutInflater)
@@ -67,15 +52,6 @@ public class NavDrawerListAdapter extends BaseAdapter {
         }else{
             imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
             txtTitle.setText(navDrawerItems.get(position).getTitle());
-        }
-
-        // displaying count
-        // check whether it set visible or not
-        if(navDrawerItems.get(position).getCounterVisibility()){
-            //txtCount.setText(navDrawerItems.get(position).getCount());
-        }else{
-            // hide the counter view
-            //txtCount.setVisibility(View.GONE);
         }
 
         return convertView;
@@ -115,4 +91,20 @@ public class NavDrawerListAdapter extends BaseAdapter {
         return output;
 
     }
+
+    @Override
+    public int getCount() {
+        return navDrawerItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return navDrawerItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
 }
